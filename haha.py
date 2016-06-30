6790e00c29630a08e78d667fa455ea28f98275a3
陈文江 <chenwenjiang@oneniceapp.com>
2016-06-30 18:47:12 +0800

    增加haha.py & other code

---
+++ haha.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#encoding:utf-8
+'''
+Author: senchen2013
+Email: senchen2013@163.com
+''' 
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import os.path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
