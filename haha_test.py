33de9a7965778caec05b2e6763b3041ddf8e29b8
陈文江 <chenwenjiang@oneniceapp.com>
2016-07-01 22:10:12 +0800

    增加testcase

--- haha_test.py
+++ haha_test.py
@@ -4,19 +4,22 @@
 Author: senchen2013 
 Email: senchen2013@163.com
 ''' 
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
 
 import haha
 
 class TestHbase2Client(object):
     
     def setUp(self):
-        self.hbase2Client = haha.Hbase2Client("bhd02", 9090, 1000)
+        self.hbase2Client = haha.Hbase2Client("localhost", 9090, 1000)
 
-    def testPut(self):
+    def test1Put(self):
         self.hbase2Client.put('test:haha', 'row1', 'f1', 'value1')
         get = self.hbase2Client.get('test:haha', 'row1')
         assert get.get("f1", None) == "value1"
+    
+    def test2Delete(self):
+        assert self.hbase2Client.exists('test:haha', 'row1', ['f1']) == True
+        self.hbase2Client.delete('test:haha', 'row1', ['f1']) 
+        assert self.hbase2Client.exists('test:haha', 'row1', ['f1']) == False
+        
     
